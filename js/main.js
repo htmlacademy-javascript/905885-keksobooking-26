a762aefc6e330a4c53e942759130d57a22969c9b
Nillud <agadullin.linar@mail.ru>
2022-06-16 23:31:09 +0500

    Тестовые данные

--- js/main.js
+++ js/main.js
@@ -1,46 +1,131 @@
-function getRandomNumber (min, max) {
-  let message = '';
+function getRandomInteger (min, max) {
+  const minNumber = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const maxNumber = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
 
-  if (min, max < 0) {
-    message = `Диапазон не может быть отрицательным, Возможно вы имели ввиду этот диапазон: (${  Math.abs(min)  },${  Math.abs(max)  }), тогда среднее значение: `;
-  }
+  const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
-  if (min >= max) {
-    return 'Неверно указан диапазон';
-  }
-
-  min = Math.ceil(Math.abs(min));
-  max = Math.floor(Math.abs(max));
-
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return (message + randomNumber);
+  return randomNumber;
 }
 
-function getRandomDotNumber (min, max, amount) {
-  let message = '';
+function getRandomFloat (min, max, amount) {
+  const minNumber = Math.min(Math.abs(min), Math.abs(max));
+  const maxNumber = Math.max(Math.abs(min), Math.abs(max));
+  const randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
 
-  if (min, max < 0) {
-    message = `Диапазон не может быть отрицательным, Возможно вы имели ввиду этот диапазон: (${  Math.abs(min)  },${  Math.abs(max)  }), тогда число будет: `;
-  }
-
-  if (min >= max) {
-    return 'Неверно указан диапазон';
-  }
-
-  min = Math.abs(min);
-  max = Math.abs(max);
-  let randomNumber = Math.random() * (max - min + 1) + min;
-
-  if (randomNumber > max) {
-    randomNumber = max;
-  }
-
-  const range = randomNumber.toFixed(amount);
-
-  return (message + range);
+  return +randomNumber.toFixed(amount);
 }
 
-getRandomNumber(-1, -5);
+getRandomInteger(-1, -5);
 
-getRandomDotNumber(1, 5, 4);
+getRandomFloat(1, 5, 4);
+
+const AVATAR = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+  'img/avatars/user09.png',
+  'img/avatars/user10.png'
+];
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+
+const generateArrayList = (elements) => {
+  const arrayLength = getRandomInteger(0, elements.length);
+  const temporaryArray = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    const arrayIndex = getRandomInteger(0, elements - 1);
+    const arrayElement = getRandomArrayElement(elements);
+
+    if (!temporaryArray.includes(arrayIndex)) {
+      temporaryArray.push(arrayElement);
+    }
+  }
+
+  return temporaryArray;
+};
+
+const generateLocation = () => ({
+  lat: getRandomFloat(35.65000, 35.70000, 5),
+  lng: getRandomFloat(139.70000, 139.80000, 5)
+});
+
+const createAuthor = () => ({
+  author: getRandomArrayElement(AVATAR)
+});
+
+const createAd = () => ({
+  title: 'Объявление!',
+  address: (generateLocation()),
+  price: getRandomInteger(10000, 100000),
+  type: getRandomArrayElement(TYPE),
+  rooms: getRandomInteger(1, 10),
+  guests: getRandomInteger(1, 10),
+  checkin: getRandomArrayElement(CHECKIN),
+  checkout: getRandomArrayElement(CHECKOUT),
+  features: generateArrayList(FEATURES),
+  description: 'Прекрасное место для вас и ваших гостей!',
+  photos: generateArrayList(PHOTOS)
+});
+
+const ADS_COUNT = 10;
+
+const createAuthorsList = () => {
+  const authorsList = Array.from({length: ADS_COUNT}, createAuthor);
+
+  return authorsList;
+};
+
+const createAdsList = () => {
+  const adsList = Array.from({length: ADS_COUNT}, createAd);
+
+  return adsList;
+};
+
+
+generateLocation();
+createAuthor();
+createAd();
+createAdsList();
+createAuthorsList();
