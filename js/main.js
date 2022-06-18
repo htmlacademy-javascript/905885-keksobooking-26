55401c74024845e58799668ad350231d104fdfcb
Nillud <agadullin.linar@mail.ru>
2022-06-18 21:49:26 +0500

    Правки константы

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
-function getRandomInteger (min, max) {
-  const minNumber = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const maxNumber = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+const ADS_COUNT = 10;
 
-  const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-
-  return randomNumber;
-}
-
-function getRandomFloat (min, max, amount) {
-  const minNumber = Math.min(Math.abs(min), Math.abs(max));
-  const maxNumber = Math.max(Math.abs(min), Math.abs(max));
-  const randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
-
-  return +randomNumber.toFixed(amount);
-}
-
-getRandomInteger(-1, -5);
-
-getRandomFloat(1, 5, 4);
-
-const AVATAR = [
+const AVATARS = [
   'img/avatars/user01.png',
   'img/avatars/user02.png',
   'img/avatars/user03.png',
@@ -32,7 +13,7 @@ const AVATAR = [
   'img/avatars/user10.png'
 ];
 
-const TYPE = [
+const TYPES = [
   'palace',
   'flat',
   'house',
@@ -40,13 +21,13 @@ const TYPE = [
   'hotel'
 ];
 
-const CHECKIN = [
+const CHECKINS = [
   '12:00',
   '13:00',
   '14:00'
 ];
 
-const CHECKOUT = [
+const CHECKOUTS = [
   '12:00',
   '13:00',
   '14:00'
@@ -67,6 +48,23 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
+function getRandomInteger (min, max) {
+  const minNumber = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const maxNumber = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+
+  const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+  return randomNumber;
+}
+
+function getRandomFloat (min, max, amount) {
+  const minNumber = Math.min(Math.abs(min), Math.abs(max));
+  const maxNumber = Math.max(Math.abs(min), Math.abs(max));
+  const randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+
+  return +randomNumber.toFixed(amount);
+}
+
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 
@@ -75,10 +73,9 @@ const generateArrayList = (elements) => {
   const temporaryArray = [];
 
   for (let i = 0; i < arrayLength; i++) {
-    const arrayIndex = getRandomInteger(0, elements - 1);
     const arrayElement = getRandomArrayElement(elements);
 
-    if (!temporaryArray.includes(arrayIndex)) {
+    if (!temporaryArray.includes(arrayElement)) {
       temporaryArray.push(arrayElement);
     }
   }
@@ -86,46 +83,37 @@ const generateArrayList = (elements) => {
   return temporaryArray;
 };
 
-const generateLocation = () => ({
-  lat: getRandomFloat(35.65000, 35.70000, 5),
-  lng: getRandomFloat(139.70000, 139.80000, 5)
-});
+const generateLocation = () => {
+  const latitide = getRandomFloat(35.65000, 35.70000, 5);
+  const longtude = getRandomFloat(139.70000, 139.80000, 5);
+
+  return {
+    lat: latitide,
+    lng: longtude
+  };
+};
 
 const createAuthor = () => ({
-  author: getRandomArrayElement(AVATAR)
+  author: getRandomArrayElement(AVATARS)
 });
 
 const createAd = () => ({
   title: 'Объявление!',
   address: (generateLocation()),
   price: getRandomInteger(10000, 100000),
-  type: getRandomArrayElement(TYPE),
+  type: getRandomArrayElement(TYPES),
   rooms: getRandomInteger(1, 10),
   guests: getRandomInteger(1, 10),
-  checkin: getRandomArrayElement(CHECKIN),
-  checkout: getRandomArrayElement(CHECKOUT),
+  checkin: getRandomArrayElement(CHECKINS),
+  checkout: getRandomArrayElement(CHECKOUTS),
   features: generateArrayList(FEATURES),
   description: 'Прекрасное место для вас и ваших гостей!',
   photos: generateArrayList(PHOTOS)
 });
 
-const ADS_COUNT = 10;
+const createAuthorsList = () => Array.from({length: ADS_COUNT}, createAuthor);
 
-const createAuthorsList = () => {
-  const authorsList = Array.from({length: ADS_COUNT}, createAuthor);
+const createAdsList = () => Array.from({length: ADS_COUNT}, createAd);
 
-  return authorsList;
-};
-
-const createAdsList = () => {
-  const adsList = Array.from({length: ADS_COUNT}, createAd);
-
-  return adsList;
-};
-
-
-generateLocation();
-createAuthor();
-createAd();
 createAdsList();
 createAuthorsList();
